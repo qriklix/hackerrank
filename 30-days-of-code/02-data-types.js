acf5781f06d0becc472310b00e9b0b0debd0d92e
qriklix <qriklix@zergling.hu>
2018-01-02 09:37:08 +0100

    30 days 02

---
+++ 30-days-of-code/02-data-types.js
@@ -0,0 +1,7 @@
+let integer = readLine()
+let double = readLine()
+let string = readLine()
+
+console.log(i + parseInt(integer))
+console.log((d + parseFloat(double)).toFixed(1))
+console.log(s + string)
