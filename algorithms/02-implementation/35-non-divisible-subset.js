13b6b7a4c11e003174a3308e25f56d54a321d358
qriklix <qriklix@zergling.hu>
2018-01-14 03:52:21 +0100

    algorithms implementation 35

---
+++ algorithms/02-implementation/35-non-divisible-subset.js
@@ -0,0 +1,22 @@
+function nonDivisibleSubset(k, arr) {
+  let arrSize = arr.length
+
+  moduli = new Array(k).fill(0)
+
+  for (let i = 0; i < arrSize; i++) {
+    modulus = arr[i] % k
+    moduli[modulus]++
+  }
+
+  let count = moduli[0] > 0 ? 1 : 0
+
+  for (let i = 1; i < k / 2; i++) {
+    count += Math.max(moduli[i], moduli[k - i])
+  }
+
+  if (k % 2 === 0) {
+    count += moduli[k / 2]
+  }
+
+  return count
+}
