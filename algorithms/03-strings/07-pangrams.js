e9ae161a9ffd2818e5c9331a472fcb1f6eb75db8
qriklix <qriklix@zergling.hu>
2018-01-26 03:37:14 +0100

    algorithms strings 07

--- algorithms/03-strings/07-pangrams.js
+++ algorithms/03-strings/07-pangrams.js
@@ -1,10 +1,19 @@
-let s = input.toLowerCase();
-let neededLetters = 'abcdefghijklmnopqrstuvwxyz'.split('')
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+var input = "";
+process.stdin.on("data", function (chunk) {
+    input += chunk;
+});
+process.stdin.on("end", function () {
+  let s = input.toLowerCase();
+  let neededLetters = 'abcdefghijklmnopqrstuvwxyz'.split('')
 
-for (let i = 0; i < s.length; i++) {
-  if (neededLetters.includes(s[i])) {
-    neededLetters.splice(neededLetters.indexOf(s[i]), 1)
+  for (let i = 0; i < s.length; i++) {
+    if (neededLetters.includes(s[i])) {
+      neededLetters.splice(neededLetters.indexOf(s[i]), 1)
+    }
   }
-}
 
-console.log(neededLetters.length === 0 ? 'pangram' : 'not pangram')
+  console.log(neededLetters.length === 0 ? 'pangram' : 'not pangram')
+
+});
