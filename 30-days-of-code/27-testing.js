2a24c2dbe0b56b86888d9784bbd613c1945a82ee
qriklix <qriklix@zergling.hu>
2018-01-30 01:25:56 +0100

    30 days 27

--- 30-days-of-code/27-testing.js
+++ 30-days-of-code/27-testing.js
@@ -1,16 +1,16 @@
 console.log(5)
 
 console.log('4 3')
-console.log('-1 0 -1 -2')
+console.log('-1 0 1 1')
 
-console.log('1 0')
-console.log('4')
+console.log('6 1')
+console.log('-1 0 -1 0 1 1')
 
 console.log('5 3')
-console.log('0 -1 5 6 7')
+console.log('0 -1 1 1 1')
 
 console.log('3 2')
-console.log('-9 0 10')
+console.log('-1 0 1')
 
 console.log('7 6')
-console.log('-9 -8 -7 0 9 10 11')
+console.log('-1 -1 -1 0 1 1 1')
