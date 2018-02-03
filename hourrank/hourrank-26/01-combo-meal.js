a2dcf42e8c46817530368d9ccdd6a33fd85a5b39
qriklix <qriklix@zergling.hu>
2018-02-03 01:04:01 +0100

    fix

--- hourrank/hourrank-26/01-combo-meal.js
+++ hourrank/hourrank-26/01-combo-meal.js
@@ -1,7 +1,11 @@
 function profit(b, s, c) {
+  return b + s - c
+  /*
+  // brute force b-i+s-i = c-i ---> b+s-2i = c-i ---> b+s-c=i
   for (let i = 0; i <= 5000; i++) {
     if ((b - i) + (s - i) === c - i) {
       return i
     }
   }
+  */
 }
